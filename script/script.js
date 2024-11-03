20379d7f99ed434630debcf17a4b2d88946a0053
BarucoMarco2091 <108827736+BarucoMarco2091@users.noreply.github.com>
2024-11-03 16:23:15 -0300

    i

--- script/script.js
+++ script/script.js
@@ -102,3 +102,38 @@ const data = [
         }
     }
 ]
+
+const weeklyButton = document.querySelector('.btn-weekly');
+weeklyButton.addEventListener('click', function weekly() {
+    const newGridContainer = document.getElementById('grid-container');
+    newGridContainer.innerHTML = '';
+    data.forEach((item) => {
+        const newDiv = document.createElement('div');
+        newDiv.className = 'new-grid';
+        newDiv.innerHTML =
+        `
+       <p>${item.title}</p>
+       <p>${item.timeframes.weekly.current}</p>
+       <p>${item.timeframes.weekly.previous}</p>
+       `
+        newGridContainer.appendChild(newDiv);
+    });
+});
+
+const monthlyButton = document.querySelector('.btn-monthly');
+monthlyButton.addEventListener('click', function monthly() {
+    const newGridContainer = document.getElementById('grid-container');
+    newGridContainer.innerHTML = '';
+    data.forEach((item) => {
+        const newDiv = document.createElement('div');
+        newDiv.className = 'new-grid';
+        newDiv.innerHTML =
+        `
+        <p>${item.title}</p>
+        <p>${item.timeframes.monthly.current}</p>
+        <p>${item.timeframes.monthly.previous}</p>
+        `
+        newGridContainer.appendChild(newDiv);
+    });
+});
+
